1f442484f2d0933895ce82c4df80597f9aeb3e93
Raomengnan <raomengnan@foxmail.com>
2016-12-02 01:31:21 +0800

    创建Provider的测试

--- WF-Provider/src/main/java/org/radrso/demo/impl/test/Test1Impl.java
+++ WF-Provider/src/main/java/org/radrso/demo/impl/test/Test1Impl.java
@@ -1,10 +1,13 @@
 package org.radrso.demo.impl.test;
 
 import org.radrso.demo.test.Test1;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by raomengnan on 16-12-2.
  */
+@Service
 public class Test1Impl implements Test1 {
     @Override
     public String test1() {
--- WF-Provider/src/main/java/org/radrso/demo/impl/test/Test2Impl.java
+++ WF-Provider/src/main/java/org/radrso/demo/impl/test/Test2Impl.java
@@ -1,10 +1,12 @@
 package org.radrso.demo.impl.test;
 
 import org.radrso.demo.test.Test2;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by raomengnan on 16-12-2.
  */
+@Service
 public class Test2Impl implements Test2 {
     @Override
     public String test2() {
