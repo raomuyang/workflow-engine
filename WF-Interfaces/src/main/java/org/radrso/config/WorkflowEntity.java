198c7d085000e0606d9c23a0ba42671522a1a281
Raomengnan <raomengnan@foxmail.com>
2016-12-09 00:03:17 +0800

    定义工作流配置文件和实体类范式

--- WF-Interfaces/src/main/java/org/radrso/config/WorkflowEntity.java
+++ WF-Interfaces/src/main/java/org/radrso/config/WorkflowEntity.java
@@ -1,9 +1,22 @@
 package org.radrso.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.radrso.config.items.DoOnNext;
+import org.radrso.config.items.InputItem;
+
 /**
  * Created by raomengnan on 16-12-8.
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WorkflowEntity {
     private String application;
     private String workflow;
+    private InputItem[] inputItems;
+    private String header;
+    private DoOnNext[] run;
 }
