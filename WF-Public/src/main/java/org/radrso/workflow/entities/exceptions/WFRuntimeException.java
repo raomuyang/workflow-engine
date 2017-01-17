a616ed0f78d07c2582d78c2d4fe799ef4b3ac82f
Raomengan <raomengnan@qq.com>
2017-01-17 20:14:06 +0800

    添加构造方法

--- WF-Public/src/main/java/org/radrso/workflow/entities/exceptions/WFRuntimeException.java
+++ WF-Public/src/main/java/org/radrso/workflow/entities/exceptions/WFRuntimeException.java
@@ -7,4 +7,8 @@ public class WFRuntimeException extends RuntimeException{
     public WFRuntimeException(String msg){
         super(msg);
     }
+
+    public WFRuntimeException(String message, Throwable e) {
+        super(message, e);
+    }
 }
