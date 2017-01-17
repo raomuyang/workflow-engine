d80ce933a4d5d57fd659c58902431b62001634a5
Raomengan <raomengnan@qq.com>
2017-01-17 22:04:58 +0800

    将实体类对应到mongoDB的集合中

--- WF-Public/src/main/java/org/radrso/workflow/entities/wf/WorkflowInstance.java
+++ WF-Public/src/main/java/org/radrso/workflow/entities/wf/WorkflowInstance.java
@@ -3,6 +3,7 @@ package org.radrso.workflow.entities.wf;
 import lombok.Data;
 import lombok.ToString;
 import org.radrso.workflow.entities.response.WFResponse;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -13,6 +14,7 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @Data
 @ToString
+@Document(collection = "workflowInstance")
 public class WorkflowInstance implements Serializable{
     public static final String EXPIRED = "expired";
     public static final String RUNNING = "running";
