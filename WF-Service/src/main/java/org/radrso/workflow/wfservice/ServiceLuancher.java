e48819a3ca7ad2e41f774351dee451f7e0ddb4db
Raomengan <raomengnan@qq.com>
2017-01-19 15:51:27 +0800

    Springboot Service luancher

--- WF-Service/src/main/java/org/radrso/workflow/wfservice/ServiceLuancher.java
+++ WF-Service/src/main/java/org/radrso/workflow/wfservice/ServiceLuancher.java
@@ -3,13 +3,18 @@ package org.radrso.workflow.wfservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
 
 /**
  * Created by raomengnan on 17-1-18.
  */
-@SpringBootApplication(scanBasePackages = {"org.radrso.workflow.wfservice.repositories"})
+@SpringBootApplication(scanBasePackages = {"org.radrso.workflow.wfservice"})
+@ImportResource("classpath:dubbo-consumer.xml")
 @EnableAutoConfiguration
 public class ServiceLuancher {
+
+
     public static void main(String[] args) {
         SpringApplication.run(ServiceLuancher.class, args);
     }
