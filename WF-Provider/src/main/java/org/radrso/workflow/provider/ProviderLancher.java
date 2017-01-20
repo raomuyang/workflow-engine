05e0040608071ee92bf497643ac0ec5e15fdec9f
Raomengan <raomengnan@qq.com>
2017-01-20 17:24:49 +0800

    删除旧Spring配置文件，去除Provider启动器中的RequestMapping

--- WF-Provider/src/main/java/org/radrso/workflow/provider/ProviderLancher.java
+++ WF-Provider/src/main/java/org/radrso/workflow/provider/ProviderLancher.java
@@ -4,22 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by raomengnan on 17-1-18.
  */
 @ImportResource("classpath:dubbo-provider.xml")
 @SpringBootApplication(scanBasePackages = {"org.radrso.workflow.provider"})
-@RestController
 @EnableAutoConfiguration
 public class ProviderLancher {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
     public static void main(String[] args) {
         SpringApplication.run(ProviderLancher.class, args);
 
