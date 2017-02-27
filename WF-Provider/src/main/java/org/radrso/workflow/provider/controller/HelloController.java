e6f49a59452d1f3476229b3df33687557163d5e0
raomengnan <raomengnan@qq.com>
2017-02-27 17:25:35 +0800

    Add "say hello"

---
+++ WF-Provider/src/main/java/org/radrso/workflow/provider/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.radrso.workflow.provider.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by rao-mengnan on 2017/2/27.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
