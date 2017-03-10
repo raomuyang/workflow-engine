056d04a3243534382be88848e24a3e0d436f3c6b
raomengnan <raomengnan@qq.com>
2017-03-11 01:09:53 +0800

    添加类的描述信息

--- WF-Public/src/main/java/org/radrso/workflow/entities/config/items/Transfer.java
+++ WF-Public/src/main/java/org/radrso/workflow/entities/config/items/Transfer.java
@@ -12,7 +12,10 @@ import java.util.List;
 /**
  * Created by raomengnan on 16-12-8.
  * 状态转移函数
- * wait 表示等待的时间
+ * judge    内部的判断函数
+ * deadline 截止时间
+ * to       下一个Step的sign（id）
+ * scatters 从此处产生新的分支
  */
 
 @Data
