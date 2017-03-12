4f0480b45a22824c70dbc5b7f8f0382bc8ae9b91
raomengnan <raomengnan@qq.com>
2017-03-12 14:34:13 +0800

    移动unittest文件的目录

--- WF-Plugins/src/main/test/TestDir.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by raomengnan on 17-1-2.
- */
-public class TestDir {
-    public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
-    }
-}
