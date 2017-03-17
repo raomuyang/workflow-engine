c0acb7ae14b88be7860ae12b318363157407d756
raomengnan <raomengnan@qq.com>
2017-03-17 17:53:03 +0800

    Demo

---
+++ WF-Service/simple-demo/src/main/java/org/radrso/workflow/jobdemo/Hello.java
@@ -0,0 +1,19 @@
+package org.radrso.workflow.jobdemo;
+
+/**
+ * Created by rao-mengnan on 2017/3/17.
+ */
+public class Hello {
+    public String hello(String name){
+        return "hello," + name;
+    }
+
+    public String wakeup(String name){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return String.format("Already wake %s up", name);
+    }
+}
